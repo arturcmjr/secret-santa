d51b651ae0d3a9cea36cb976732b1d256524c1ea
Artur Junior <arturcmjr@gmail.com>
2022-04-19 09:50:56 -0300

    angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { ModulesModule } from './modules/modules.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ModulesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
