413d66f6fe4984558c72d22bf2f16b7c505b8763
Artur Junior <69425765+arturcmjr@users.noreply.github.com>
2024-11-26 17:38:51 -0300

    removing old reveal page

--- src/app/modules/modules.module.ts
+++ src/app/modules/modules.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home/pages/home/home.component';
 import { CreateSecretSantaComponent } from './secret-santa/pages/create-secret-santa/create-secret-santa.component';
-import { RevealSecretSantaComponent } from './secret-santa/pages/reveal-secret-santa/reveal-secret-santa.component';
 import { ListParticipantsComponent } from './secret-santa/pages/list-participants/list-participants.component';
 import { ErrorIllustrationComponent } from './secret-santa/components/error-illustration/error-illustration.component';
 import { ParticipantModule } from './secret-santa/pages/participant/participant.module';
@@ -13,7 +12,6 @@ import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
   declarations: [
     HomeComponent,
     CreateSecretSantaComponent,
-    RevealSecretSantaComponent,
     ListParticipantsComponent,
     ErrorIllustrationComponent,
   ],
